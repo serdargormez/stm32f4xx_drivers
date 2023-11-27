e3579539455cc03e787a6db45c753c6b3169b8b9
Serdar Gormez <hsgormez@gmail.com>
2023-11-27 15:43:44 +0300

    Added spi_tx.testing.c file and SPI2_GPIOInit function

---
+++ Src/spi_tx_testing.c
@@ -0,0 +1,52 @@
+/*
+ * spi_tx_testing.c
+ *
+ *  Created on: 27 Kas 2023
+ *      Author: hsgor
+ */
+
+#include "stm32f411xx.h"
+
+/*
+PB12 -> SPI2_NSS
+PB13 -> SPI2_SCLK
+PB14 -> SPI2_MISO
+PB15 -> SPI2_MOSI
+Alternate function mode : 5
+*/
+
+
+void SPI2_GPIOInits(void)
+{
+	GPIO_Handle_t SPIPins;
+
+	SPIPins.pGPIOx = GPIOB;
+	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	//SCLK
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+
+	//MOSI
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&SPIPins);
+
+	//MISO
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	GPIO_Init(&SPIPins);
+
+	//NSS
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&SPIPins);
+}
+
+int main(void)
+{
+	SPI2_GPIOInits();
+
+	return 0;
+}
