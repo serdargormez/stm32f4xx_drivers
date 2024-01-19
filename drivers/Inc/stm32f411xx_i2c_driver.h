74aa3186818ef42957a79663156af46d5153d561
Serdar Gormez <hsgormez@gmail.com>
2024-01-19 13:15:00 +0300

    Edited I2C application event macros. Moved I2C_GenerateStopCondition function.

--- drivers/Inc/stm32f411xx_i2c_driver.h
+++ drivers/Inc/stm32f411xx_i2c_driver.h
@@ -93,6 +93,11 @@ typedef struct
 #define I2C_EV_TX_CMPLT						0
 #define I2C_EV_RX_CMPLT						1
 #define I2C_EV_STOP							2
+#define I2C_ERROR_BERR						3
+#define I2C_ERROR_ARLO						4
+#define I2C_ERROR_AF						5
+#define I2C_ERROR_OVR						6
+#define I2C_ERROR_TIMEOUT					7
 
 
 /*Peripheral clock setup */
@@ -123,6 +128,7 @@ void I2C_CloseReceiveData(I2C_Handle_t *pI2CHandle);
 
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
 void I2C_ManageAcking(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+void I2C_GenerateStopCondition(I2C_RegDef_t *pI2Cx);
 
 
 /*Flag Status*/
