ccc46d3ff85506c30e8b634f7741ba98084ea3d0
Serdar Gormez <hsgormez@gmail.com>
2024-02-18 22:43:10 +0300

    Edited USART_Init. Added definitions of USART_SendData, USART_ReceiveData, USART_SendDataIT, USART_ReceiveDataIT, USART_PeripheralControl, USART_SetBaudRate functions.

--- drivers/Src/stm32f411xx_usart_driver.c
+++ drivers/Src/stm32f411xx_usart_driver.c
@@ -167,7 +167,7 @@ void USART_Init(USART_Handle_t *pUSARTHandle)
 /******************************** Configuration of BRR(Baudrate register)******************************************/
 
 	//Implement the code to configure the baud rate
-
+	USART_SetBaudRate(pUSARTHandle->pUSARTx, pUSARTHandle->USART_Config.USART_Baud);
 
 }
 
@@ -180,29 +180,159 @@ void USART_DeInit(USART_RegDef_t *pUSARTx)
 
 /*Data Send and Receive*/
 
-void USART_SendData(USART_RegDef_t *pUSARTx,uint8_t *pTxBuffer, uint32_t Len)
+void USART_SendData(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, uint32_t Len)
 {
+	uint16_t *pdata;
+	uint8_t  *pdata2;
+	//Loop over until "Len" number of bytes are transferred
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+		//Implement the code to wait until TXE flag is set in the SR
+		while(!USART_GetFlagStatus(pUSARTHandle->pUSARTx, USART_FLAG_TXE));
 
+         //Check the USART_WordLength item for 9BIT or 8BIT in a frame
+		if(pUSARTHandle->USART_Config.USART_WordLength == USART_WORDLEN_9BITS)
+		{
+			//if 9BIT, load the DR with 2bytes masking the bits other than first 9 bits
+			pdata = (uint16_t*) pTxBuffer;
+			pUSARTHandle->pUSARTx->DR = (*pdata & (uint16_t)0x01FF);
+
+			//check for USART_ParityControl
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used in this transfer. so, 9bits of user data will be sent
+				//Implement the code to increment pTxBuffer twice
+				pTxBuffer++;
+				pTxBuffer++;
+			}
+
+			else
+			{
+				//Parity bit is used in this transfer . so , 8bits of user data will be sent
+				//The 9th bit will be replaced by parity bit by the hardware
+				pTxBuffer++;
+			}
+		}
+
+		else
+		{
+			pdata2 = pTxBuffer;
+			//This is 8bit data transfer
+			pUSARTHandle->pUSARTx->DR = (uint8_t)(*pdata2 & 0xFFU);
+
+			//Implement the code to increment the buffer address
+			pdata2++;
+		}
+	}
+
+	//Implement the code to wait till TC flag is set in the SR
+	while( ! USART_GetFlagStatus(pUSARTHandle->pUSARTx,USART_FLAG_TC));
 }
 
 
-void USART_ReceiveData(USART_RegDef_t *pUSARTx, uint8_t *pRxBuffer, uint32_t Len)
+void USART_ReceiveData(USART_Handle_t *pUSARTHandle, uint8_t *pRxBuffer, uint32_t Len)
 {
+	//Loop over until "Len" number of bytes are transferred
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+		//Implement the code to wait until RXNE flag is set in the SR
+		while(!USART_GetFlagStatus(pUSARTHandle->pUSARTx, USART_FLAG_RXNE));
 
+		//Check the USART_WordLength to decide whether we are going to receive 9bit of data in a frame or 8 bit
+		if(pUSARTHandle->USART_Config.USART_WordLength == USART_WORDLEN_9BITS)
+		{
+			//We are going to receive 9bit data in a frame
+
+			//check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used. so, all 9bits will be of user data
+
+				//read only first 9 bits. so, mask the DR with 0x01FF
+				*((uint16_t*) pRxBuffer) = (pUSARTHandle->pUSARTx->DR  & (uint16_t)0x01FF);
+
+				//Now increment the pRxBuffer two times
+				pRxBuffer++;
+				pRxBuffer++;
+			}
+
+			else
+			{
+				//Parity is used, so, 8bits will be of user data and 1 bit is parity
+				*pRxBuffer = (pUSARTHandle->pUSARTx->DR  & (uint8_t)0xFF);
+
+				 //Increment the pRxBuffer
+				pRxBuffer++;
+			}
+		}
+
+		else
+		{
+			//We are going to receive 8bit data in a frame
+
+			//check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used , so all 8bits will be of user data
+
+				//read 8 bits from DR
+				 *pRxBuffer = pUSARTHandle->pUSARTx->DR;
+			}
+
+			else
+			{
+				//Parity is used, so , 7 bits will be of user data and 1 bit is parity
+
+				//read only 7 bits , hence mask the DR with 0X7F
+				 *pRxBuffer = (pUSARTHandle->pUSARTx->DR  & (uint8_t)0X7F);
+			}
+
+			//increment the pRxBuffer
+			pRxBuffer++;
+		}
+	}
 }
 
-/*
+
 uint8_t USART_SendDataIT(USART_Handle_t *pUSARTHandle,uint8_t *pTxBuffer, uint32_t Len)
 {
+	uint8_t txstate = pUSARTHandle->TxBusyState;
 
+	if(txstate != USART_BUSY_IN_TX)
+	{
+		pUSARTHandle->TxLen = Len;
+		pUSARTHandle->pTxBuffer = pTxBuffer;
+		pUSARTHandle->TxBusyState = USART_BUSY_IN_TX;
+
+		//Implement the code to enable interrupt for TXE
+		pUSARTHandle->pUSARTx->CR1 |= (1 << USART_CR1_TXEIE);
+
+
+		//Implement the code to enable interrupt for TC
+		pUSARTHandle->pUSARTx->CR1 |= (1 << USART_CR1_TCIE);
+	}
+
+	return txstate;
 }
 
 
 uint8_t USART_ReceiveDataIT(USART_Handle_t *pUSARTHandle, uint8_t *pRxBuffer, uint32_t Len)
 {
+	uint8_t rxstate = pUSARTHandle->RxBusyState;
 
+	if(rxstate != USART_BUSY_IN_RX)
+	{
+		pUSARTHandle->RxLen = Len;
+		pUSARTHandle->pRxBuffer = pRxBuffer;
+		pUSARTHandle->RxBusyState = USART_BUSY_IN_RX;
+
+		//Implement the code to enable interrupt for RXNE
+		pUSARTHandle->pUSARTx->CR1 |= (1 << USART_CR1_RXNEIE);
+	}
+
+	return rxstate;
 }
-*/
+
 
 /*IRQ Configuration and ISR handling*/
 
@@ -228,7 +358,15 @@ void USART_IRQHandling(USART_Handle_t *pHandle)
 
 void USART_PeripheralControl(USART_RegDef_t *pUSARTx, uint8_t EnOrDi)
 {
+	if(EnOrDi == ENABLE)
+	{
+		pUSARTx->CR1 |= (1 << USART_CR1_UE);
+	}
 
+	else
+	{
+		pUSARTx->CR1 &= ~(1 << USART_CR1_UE);
+	}
 }
 
 
@@ -251,6 +389,75 @@ uint8_t USART_GetFlagStatus(USART_RegDef_t *pUSARTx , uint32_t FlagName)
 void USART_ClearFlag(USART_RegDef_t *pUSARTx, uint16_t StatusFlagName)
 {
 
+}
+
+
+void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate)
+{
+	//Variable to hold the APB clock
+	uint32_t PCLKx;
+
+	uint32_t usartdiv;
+
+	//variables to hold Mantissa and Fraction values
+	uint32_t M_part, F_part;
+
+	uint32_t tempreg = 0;
+
+	//Get the value of APB bus clock in to the variable PCLKx
+	if(pUSARTx == USART1 || pUSARTx == USART6)
+	{
+	   //USART1 and USART6 are hanging on APB2 bus
+	   PCLKx = RCC_GetPCLK2Value();
+	}
+
+	else
+	{
+	   PCLKx = RCC_GetPCLK1Value();
+	}
+
+	//Check for OVER8 configuration bit
+	if(pUSARTx->CR1 & (1 << USART_CR1_OVER8))
+	{
+	   //OVER8 = 1 , over sampling by 8
+	   usartdiv = ((25 * PCLKx) / (2 * BaudRate));
+	}
+
+	else
+	{
+	   //over sampling by 16
+		usartdiv = ((25 * PCLKx) / (4 * BaudRate));
+	}
+
+	//Calculate the Mantissa part
+	M_part = usartdiv / 100;
+
+	//Place the Mantissa part in appropriate bit position . refer USART_BRR
+	tempreg |= M_part << 4;
+
+	//Extract the fraction part
+	F_part = (usartdiv - (M_part * 100));
+
+	//Calculate the final fractional
+	if(pUSARTx->CR1 & ( 1 << USART_CR1_OVER8))
+	{
+	  //OVER8 = 1 , over sampling by 8
+	  F_part = (((F_part * 8)+ 50) / 100) & ((uint8_t)0x07);
+
+	}
+
+	else
+	{
+	   //over sampling by 16
+	   F_part = (((F_part * 16)+ 50) / 100) & ((uint8_t)0x0F);
+
+	}
+
+	//Place the fractional part in appropriate bit position . refer USART_BRR
+	tempreg |= F_part;
+
+	//copy the value of tempreg in to BRR register
+	pUSARTx->BRR = tempreg;
 }
 
 
